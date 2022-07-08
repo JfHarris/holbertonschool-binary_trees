344449a282fcf89cab1b0e47f25993f4d7f10b65
JFHarris <JFHarris1983@gmail.com>
2022-07-07 23:10:12 -0500

    Betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,10 +11,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		size_t l;
-        size_t r;
+		size_t r;
 
-        l = 0;
-        r = 0;
+		l = 0;
+		r = 0;
 
 		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
