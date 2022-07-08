1adbc54bd1c790423892285562b5f62af23aaebe
JFHarris <JFHarris1983@gmail.com>
2022-07-07 20:33:24 -0500

    First edit

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,5 +8,15 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaf;
 
+	leaf = 0;
+
+	if (tree)
+	{
+		leaf += (!tree->left && !tree->right) ? 1 : 0;
+		leaf += binary_tree_leaves(tree->left);
+		leaf += binary_tree_leaves(tree->right);
+	}
+	return (leaf);
 }
--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,5 +8,15 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t parent;
 
+	parent = 0;
+
+	if (tree)
+	{
+		parent += (tree->left || tree->right) ? 1 : 0;
+		parent += binary_tree_nodes(tree->left);
+		parent += binary_tree_nodes(tree->right);
+	}
+	return (parent);
 }
