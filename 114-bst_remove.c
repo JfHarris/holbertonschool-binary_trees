21138685251e7e3be26f26ff7e285651cd38f995
JFHarris <JFHarris1983@gmail.com>
2022-07-12 23:24:19 -0500

    Updated header and added 110-115

---
+++ 114-bst_remove.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+ * bst_remove - removes a node from a Binary Search Tree
+ * @root: pointer to the root node where you will remove a node
+ * @value: value to remove in the tree
+ * Return:  pointer to new root node of the tree after removing the value
+ */
+
+bst_t *bst_remove(bst_t *root, int value)
