40cd27bbe609eb14beab2345e1b2b5be06953e1f
JFHarris <JFHarris1983@gmail.com>
2022-07-14 23:03:02 -0500

    Betty

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -10,15 +10,15 @@
 bst_t *array_to_bst(int *array, size_t size)
 {
 	size_t x;
-    size_t y;
-    bst_t *tree;
+	size_t y;
+	bst_t *tree;
 
-    tree = NULL;
+	tree = NULL;
 
 	if (array == NULL)
-    {
+	{
 		return (NULL);
-    }
+	}
 	for (x = 0; x < size; x++)
 	{
 		for (y = 0; y < x; y++)
@@ -29,9 +29,9 @@ bst_t *array_to_bst(int *array, size_t size)
 		if (y == x)
 		{
 			if (bst_insert(&tree, array[x]) == NULL)
-            {
+			{
 				return (NULL);
-            }
+			}
 		}
 	}
 	return (tree);
